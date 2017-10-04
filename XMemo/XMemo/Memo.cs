61499f2c532567d878e0e70875407165f9977a59
TAKIO-PC\takio <taka21centry@gmail.com>
2017-10-05 06:30:25 +0900

    途中。画面作成前

---
+++ XMemo/XMemo/Memo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMemo
+{
+    class Memo
+    {
+        public DateTime Date{get;set;}
+        public string Subject { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ XMemo/XMemo/MemoHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMemo
+{
+    class MemoHolder
+    {
+        public static MemoHolder Current { get; } = new MemoHolder();
+        public Memo memo { get; set; }
+    }
+}
